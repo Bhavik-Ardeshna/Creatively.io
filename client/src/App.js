3f4b3c7facdac393372edce08d498e63045faf79
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-07-10 13:19:15 +0530

    added temp home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
 
 import './App.css';
+import { Logo } from './assets/svg/SVGs';
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <div class="px-20 py-6">
+        <div class="flex items-center justify-between">
+          <div class="flex items-center justify-center">
+            <div class="flex items-center justify-center text-3xl font-bold text-true-gray-800">
+              <Logo />
+                Creatively
+              </div>
+            <div class="hidden lg:flex items-center justify-center antialiased lg:ml-20 pt-1">
+              <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                Product
+                  <svg class="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
+                </svg>
+              </a>
+              <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                Community
+                  <svg class="w-3.5 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path>
+                </svg>
+              </a>
+              <a href="#" class="flex items-center justify-center mr-10 text-base text-gray-700 text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out">
+                Plans
+                </a>
+            </div>
+          </div>
+          <div class="hidden md:flex items-center justify-center">
+            <a href="#" class="mr-5 text-lg font-medium text-true-gray-800 hover:text-cool-gray-700 transition duration-150 ease-in-out">Login</a>
+            <button class="px-6 py-3 rounded-3xl font-medium bg-gradient-to-b from-gray-900 to-black text-white outline-none focus:outline-none hover:shadow-md hover:from-true-gray-900 transition duration-200 ease-in-out">Sign Up</button>
+          </div>
+        </div>
+
+        <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
+          <div class="text-6xl font-semibold text-gray-900 leading-none">Bring all your work together</div>
+          <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">A better experience for yout attendees and less stress yout team.</div>
+          <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
+            Download for Free
+            </button>
+        </div>
+
+      </div>
+    </div>
+
   );
 }
 
