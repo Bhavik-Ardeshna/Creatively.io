2f184526df4fe6111e5cbb46261aa5386cf0a26e
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-09-07 00:40:30 +0530

    added secured routes for login

--- server.js
+++ server.js
@@ -2,25 +2,18 @@ const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 
+require('./db/connect');
+
+require('./models/auth');
+
 const app = express();
 const PORT = process.env.PORT || 8800;
 
+app.use(express.json());
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
-    const data = "<h1>Welcome to the Creatively API</h1> "
-    res.send(data);
-})
-
-app.get('/api', (req, res) => {
-    const data = {
-        username: "Bhavik Ardeshna",
-        age: 56
-    };
-    res.json(data);
-})
-
+app.use('/auth',require('./routes/auth'));
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT : ${PORT} `);
