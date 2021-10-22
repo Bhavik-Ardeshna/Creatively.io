b57b1caf5dd2f614367185fbc0ec6507583f4e5e
Manan <manangoria8840@gmail.com>
2021-10-22 22:01:06 +0530

    about us page added

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import {UserContext} from '../App'
+
 
 function Profile() {
+    const {state,dispatch} = useContext(UserContext);
+
     return (
         <div>
             <h1>Profile</h1>
+            <h1>{state.name}</h1>  
         </div>
     );
 }
--- client/src/partials/Header.js
+++ client/src/partials/Header.js
@@ -47,14 +47,6 @@ function Header() {
                 </Link>
               </li>,
               <li>
-                <Link to="/contact" className="btn-sm text-black bg-blue-400 hover:bg-blue-500 ml-3">
-                    <span>Contact Us</span>
-                    <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                    </svg>
-                  </Link>
-              </li>,
-              <li>
                 <button type="button" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
                 onClick={(e)=>LogOut(e)}>
                   <span>Logout</span>
